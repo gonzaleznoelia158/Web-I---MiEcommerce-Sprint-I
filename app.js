d8ae2a45d125e9ef5f661f6f6ac8a4ee49cc015a
Noelia Noemí González <gonzaleznoelia158@gmail.com>
2026-04-05 01:26:09 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Configurar el motor de plantillas EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Definir la carpeta de archivos estáticos (CSS, imágenes)
+app.use(express.static(path.join(__dirname, 'assets')));
+
+// Rutas para cada vista
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/login', (req, res) => res.render('pages/login'));
+app.get('/registro', (req, res) => res.render('pages/register'));
+app.get('/producto', (req, res) => res.render('pages/product'));
+app.get('/carrito', (req, res) => res.render('pages/cart'));
+app.get('/checkout', (req, res) => res.render('pages/checkout'));
+
+// Servidor 
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
