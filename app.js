41cf8b7143345aca3bab75bdf2c7a2835f1ad9f3
elpupin <lucas@procomdigital.net>
2026-04-05 14:30:44 -0300

    Servidor funcional
    Le instale Express
    Le instale Nodemon
    Hice lo basico de HTML en login.ejs
    coloque un h1 index en index.ejs para verificar que funcione el servidor
    ahora cada vez que quieran arrancar el servidor deberia funcionar bien con el comando "nodemon app.js"

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'assets')));
 
 // Rutas para cada vista
-app.get('/', (req, res) => res.render('pages/index'));
-app.get('/login', (req, res) => res.render('pages/login'));
-app.get('/registro', (req, res) => res.render('pages/register'));
-app.get('/producto', (req, res) => res.render('pages/product'));
-app.get('/carrito', (req, res) => res.render('pages/cart'));
-app.get('/checkout', (req, res) => res.render('pages/checkout'));
+app.get('/', (req, res) => res.render('partials/pages/index'));
+app.get('/login', (req, res) => res.render('partials/pages/login'));
+app.get('/registro', (req, res) => res.render('partials/pages/register'));
+app.get('/producto', (req, res) => res.render('partials/pages/product'));
+app.get('/carrito', (req, res) => res.render('partials/pages/cart'));
+app.get('/checkout', (req, res) => res.render('partials/pages/checkout'));
 
 // Servidor 
 const PORT = 3000;
