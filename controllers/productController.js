454fca01ad3672b9c8d7fe8a973c1a310e61b980
elpupin <lucas@procomdigital.net>
2026-05-21 18:58:10 -0300

    #main-s2-us8

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 const productsService = require('../services/productsService');
+const productsData = require('../data/products.json');
 
 function showProductDetail(req, res) {
   const id = productsService.normalizeId(req.params.id);//normaliza el id para evitar errores de tipo
@@ -7,9 +8,29 @@ function showProductDetail(req, res) {
   }
   const product = productsService.getById(id); //busca el producto por id en la BD
   if (!product){
-     return res.status(404).send('Producto no encontrado');//si no se encuentra el producto, renderiza un error
+     return res.status(404).render('pages/404.ejs', { title: '404 - Página no encontrada' });//si no se encuentra el producto, renderiza un error
   }
-     res.render('pages/product', { product }); //si todo sale bien, muestra el producto correspondinte
+
+  // Aca arranca el user story 8
+
+  let relatedProducts = [];
+
+    // Verificar categoria
+  if (product.categoria) {
+      // Filtrar productos de la misma categoria menos el actual
+      relatedProducts = productsData.filter(
+        (p) => p.categoria === product.categoria && p.id !== product.id
+      );
+
+      // Si hay mas de 4, hago un random y quedan 4 nomas
+      if (relatedProducts.length > 4) {
+        relatedProducts = relatedProducts
+          .sort(() => 0.5 - Math.random()) // Ese math.random me lo dio la IA, ni idea como funca pero funca
+          .slice(0, 4); 
+      }
+    }
+
+     res.render('pages/product', { product, relatedProducts }); //si todo sale bien, muestra el producto correspondinte
 }
 
 //exporta la funcion
