8e24ac73487dfec57e18e19dfed979a0fab53edd
Noelia <gonzaleznoelia158@gmail.com>
2026-05-20 23:16:37 -0300

    migre mis cambios

--- app.js
+++ app.js
@@ -9,16 +9,47 @@ app.set('views', path.join(__dirname, 'views'));
 // Definir la carpeta de archivos estáticos (CSS, imágenes)
 app.use(express.static(path.join(__dirname, 'assets')));
 
-// Rutas para cada vista
-app.get('/', (req, res) => res.render('pages/index'));
-app.get('/login', (req, res) => res.render('pages/login'));
-app.get('/registro', (req, res) => res.render('pages/register'));
-app.get('/producto', (req, res) => res.render('pages/product'));
-app.get('/carrito', (req, res) => res.render('pages/cart'));
-app.get('/checkout', (req, res) => res.render('pages/checkout'));
+// Leer formularios
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Session para el carrito
+app.use(session({
+  secret: 'mi-secreto-para-carrito',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // localhost no necesita HTTPS
+}));
+
+//Middleware global, pq sino no se puede acceder al carrito desde el header sin hacer una consulta a la BD cada vez
+app.use((req, res, next) => {
+    try {
+        res.locals.cartTotal = cartService.getTotalPesos(req, productsService) || 0;
+    } catch (error) {
+        res.locals.cartTotal = 0; // Si tira error arranca en 0
+    }
+    next();
+});
+
+// Archivos de rutas
+const mainRouter = require('./routes/mainRoutes');
+const productsRouter = require('./routes/productRoutes');
+const { title } = require('process');
+const indexRouter = require('./routes/index');
+
+// Usar rutas
+app.use('/', mainRouter);
+app.use('/products', productsRouter);
+app.use('/', indexRouter);
 
 // Servidor 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+// Error 404
+
+app.use((req, res) => {
+  res.status(404).render('pages/404.ejs', { title: '404 - Página no encontrada' });
 });
