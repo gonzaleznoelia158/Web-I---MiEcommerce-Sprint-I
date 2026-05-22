e0e8987f23f39226cfa4819a7fb88eac70d14a36
Noelia <gonzaleznoelia158@gmail.com>
2026-05-22 16:40:49 -0300

    ...

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const session = require('express-session');
-const cartCont = require('./middleware/cartCont');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const path = require('path');
-const session = require('express-session'); 
 require('./db/database');
 
-
-
-//Services
+// Services
 const productsService = require('./services/productsService');
 const cartService = require('./services/cartService');
 
@@ -21,91 +17,60 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
 app.set('layout', 'layouts/main');
 app.set('layout extractStyles', true); 
-app.set('layout extractScripts', true); 
+app.set('layout extractScripts', true);
 
-// onfigurar layouts y extracción de CSS/JS
-app.use(expressLayouts);
-app.set('layout', 'layouts/main');
-app.set('layout extractStyles', true); 
-app.set('layout extractScripts', true); 
-
-// Definir la carpeta de archivos estáticos (CSS, imágenes)
+// Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Leer formularios
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Session para el carrito
+// Session para el carrito
 app.use(session({
-  secret: 'mi-secreto-para-carrito',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } // localhost no necesita HTTPS
+    secret: 'mi-secreto-para-carrito',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
 }));
 
-//Middleware global, pq sino no se puede acceder al carrito desde el header sin hacer una consulta a la BD cada vez
+// Middleware global del carrito
 app.use((req, res, next) => {
     try {
         res.locals.cartTotal = cartService.getTotalPesos(req, productsService) || 0;
         res.locals.cartCont = cartService.getCantidad(req, productsService) || 0;
     } catch (error) {
-        res.locals.cartTotal = 0; // Si tira error arranca en 0
+        res.locals.cartTotal = 0;
         res.locals.cartCont = 0;
     }
     next();
 });
 
-// Archivos de rutas
+// Rutas
 const mainRouter = require('./routes/mainRoutes');
 const productsRouter = require('./routes/productRoutes');
-const { title } = require('process');
 const indexRoutes = require('./routes/indexRoutes');
 const categoryRouter = require('./routes/categoryRoutes');
 
-// Usar rutas
 app.use('/', mainRouter);
 app.use('/products', productsRouter);
 app.use('/categories', categoryRouter);
 app.use('/', indexRoutes);
 
-//Session para el carrito
-app.use(session({
-  secret: 'mi-secreto-para-carrito',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } // localhost no necesita HTTPS
-}));
-
-//Middleware global, pq sino no se puede acceder al carrito desde el header sin hacer una consulta a la BD cada vez
-app.use((req, res, next) => {
-    try {
-        res.locals.cartTotal = cartService.getTotalPesos(req, productsService) || 0;
-    } catch (error) {
-        res.locals.cartTotal = 0; // Si tira error arranca en 0
-    }
-    next();
-});
-
-// Archivos de rutas
-const mainRouter = require('./routes/mainRoutes');
-const productsRouter = require('./routes/productRoutes');
-const { title } = require('process');
-
-// Usar rutas
-app.use('/', mainRouter);
-app.use('/products', productsRouter);
-
-
-// Error 500 forzado para pruebas
+// Error 500
 app.get('/test-error', (req, res, next) => {
     throw new Error('Este es un error forzado para probar el 500');
 });
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).render('pages/500'); 
+    res.status(500).render('pages/500');
 });
 
+// Error 404
+app.use((req, res) => {
+    res.status(404).render('pages/404', { title: '404 - Página no encontrada' });
+});
 
 // Servidor
 const PORT = 3000;
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -7,8 +7,11 @@ const products = require('../data/products.json');
 router.get('/', (req, res) => {
   const sort = req.query.sort || null; 
   const products = productsService.getAll(sort);
-  res.render('pages/index', {products, sort});
+    const suggestedProducts = productsService.getAll(null)
+    .sort(() => Math.random() - 0.5).slice(0, 4);
+  res.render('pages/index', {products, sort, suggestedProducts});
 });
+
 //Rutas de registro y login
 router.get('/login', (req, res) => { res.render('pages/login') });
 router.get('/register', (req, res) => { res.render('pages/register') });
@@ -18,7 +21,6 @@ router.get('/search', (req, res) => {
     if (!query) {
         return res.redirect('/');
     }
-
     const results = products.filter(p =>
         p.nombre.toLowerCase().includes(query) ||
         p.categoria.toLowerCase().includes(query) ||
@@ -27,5 +29,8 @@ router.get('/search', (req, res) => {
 
     res.render('pages/search', { results, query });
 });
+//rutas post login y register
+router.post('/register', mainController.processRegister);
+router.post('/login', mainController.processLogin);
 
 module.exports = router;
